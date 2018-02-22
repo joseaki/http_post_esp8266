ee067e06d9dbdc3bc4ec4d43e118a0dad95baf94
joseaki <joseakialanya@gmail.com>
2018-02-22 12:40:09 -0500

    add pwm for pin 0 and 2 and fixed ip

--- http_post_esp8266.ino
+++ http_post_esp8266.ino
@@ -4,7 +4,7 @@
 ESP8266WebServer server(80);
  
 const char* ssid = "Dell";
-const char* password = "sN17611)";
+const char* password = "Antonioo";
  
 void setup() {
     pinMode(2, OUTPUT);
@@ -12,6 +12,10 @@ void setup() {
     
     Serial.begin(115200);
     WiFi.begin(ssid, password);  //Connect to the WiFi network
+    IPAddress ip(192,168,137,200);   
+    IPAddress gateway(192,168,137,1);   
+    IPAddress subnet(255,255,255,0);   
+    WiFi.config(ip, gateway, subnet);
  
     while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
         delay(500);
@@ -33,20 +37,30 @@ void loop() {
 }
  
 void handleBody() { //Handler for the body path
-  if (server.hasArg("plain")== false){ //Check if body received
+  if (server.hasArg("v_right")== false){ //Check if body received
         server.send(200, "text/plain", "Body not received");
         return;
   }
   
+  String pwm_str_right = server.arg("v_right");
+  int pwm_value_right=pwm_str_right.toInt();
+  String pwm_str_left = server.arg("v_left");
+  int pwm_value_left=pwm_str_left.toInt();
+  
+  analogWrite(2,pwm_value_right);
+  analogWrite(0,pwm_value_left);
+  /*
   if(server.arg("plain")=="centro"){
     analogWrite(2, 512);
   }else{
     analogWrite(2,1023);
-  }
+  }*/
   
-  String message = "Body received:\n";
-         message += server.arg("plain");
+  String message = "right speed:\n";
+         message += server.arg("v_right");
          message += "\n";
+         message += "left speed:\n";
+         message += server.arg("v_left");
 
   server.send(200, "text/plain", message);
   Serial.println(message);
